9b145db6361b4d13663b0f873318d79ff8978d9a
Angela Chiang <achi2048@students.cs.ubc.ca>
2024-03-10 00:45:55 -0800

    login page almost completed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,53 +1,72 @@
-import React, {useState} from "react"; 
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
 export default function Login() {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const navigate = useNavigate();
 
-    const handleEmail = (event) => {
-        setEmail(event.target.value);
-        console.log("email added! " + email); 
-    };
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+    console.log("email added! " + email);
+  };
 
-    const handlePass = (event) => {
-        setPass(event.target.value);
-        console.log("password added! " + pass); 
-    };
+  const handlePass = (event) => {
+    setPass(event.target.value);
+    console.log("password added! " + pass);
+  };
 
-    const handleLogin = () => {
-        navigate("/about-us"); 
-    }
+  const handleLogin = () => {
+    navigate("/about-us");
+  };
 
-    const handleGmail = () => {
-        window.open("https://accounts.google.com/v3/signin/", "_blank");
-    }
+  const handleGmail = () => {
+    window.open("https://accounts.google.com/v3/signin/", "_blank");
+  };
 
-    return (
-        <div className="LoginContainer">
-            <form className = "Login-form">
-
-                <h1 className="Log-In">Log in</h1>
-
-                <p className="Sign-In-Text">Please sign in or sign up below</p>
-                
-                <div className="LoginContainer2">
-                    <label className="Email-Text" for = "email">Email</label>
-                    <input className="input" value = {email} onChange={handleEmail} type = "email" placeholder="youremail@gmail.com" id = "email" name = "email" />
-                </div>
-                
-                {/* <label for="password">password</label> */}
-                {/* <input className="pass" value = {pass} onChange={handlePass} type = "password" placeholder="password" id = "password" name = "password" /> */}
-                
-                
-                <button className="EmailButton"onClick={handleLogin} >Continue with Email</button>
-                <button className="GoogleButton"onClick={handleGmail}>Sign in with Google</button>
-            </form>
+  return (
+    <div className="LoginContainer">
+      <div className="LoginHeader">
+        <div id="LoginLogo"></div>
+        <div id="LoginSigninButton"></div>
+      </div>
+      <div className="LoginBody">
+        <div id="LoginFormDiv">
+          <div id="formLogo"></div>
+          <div id="loginTextDiv">
             
-            {/* <button>Already have an account?</button> */}
-        </div>
+          <h1 className="Log-In">Log in</h1>
+          </div>
+          <div id="signInTextDiv">
+            <p className="Sign-In-Text">Please sign in or sign up below</p>
+          </div>
+          <form className="Login-form">
+            <div className="LoginContainer2">
+              <label className="Email-Text" for="email">
+                Email
+              </label>
+              <input
+                className="input"
+                value={email}
+                onChange={handleEmail}
+                type="email"
+                placeholder="youremail@gmail.com"
+                id="email"
+                name="email"
+              />
+            </div>
 
-    );
+            <button className="EmailButton" onClick={handleLogin}>
+              Continue with Email
+            </button>
+            <button className="GoogleButton" onClick={handleGmail}>
+              Sign in with Google
+            </button>
+          </form>
+        </div>
+      </div>
+
+      {/* <button>Already have an account?</button> */}
+    </div>
+  );
 }
