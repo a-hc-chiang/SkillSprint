ac87ee3ec75c1556602e6a5f4b981261dd37cf3a
Angela Chiang <achi2048@students.cs.ubc.ca>
2024-03-09 16:13:35 -0800

    started homepage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+export default function HomePage() {
+    return (
+        <div>
+            <title>skillsumo</title>
+            <h1>Bringing a world of hobbies, skills,
+                and friendships in front of you.</h1>
+            <a href={'/'} style={{color: "black"}}>Back to Main Page</a>
+            
+        </div>
+    );
+}
